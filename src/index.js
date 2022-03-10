172a40481306049123c2c613009e49086ca3bd1a
@s_zhsn <s.zhosan@gmail.com>
2022-03-10 20:54:48 +0200

    h-w-11-process

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import './css/styles.css';
+import 'simplelightbox/dist/simple-lightbox.css';
 import PhotoApiService from './js/photo-service';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+
 const axios = require('axios');
 
 const photoApiService = new PhotoApiService();
 
-let lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250});
+let lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
 
 const refs = {
   searchForm: document.querySelector('#search-form'),
@@ -22,41 +23,41 @@ async function onSearch(e) {
   clearGallery();
   photoApiService.query = e.currentTarget.elements.searchQuery.value;
   photoApiService.resetPage();
-  createMarkup(await photoApiService.getPhotos());  
+  createMarkup(await photoApiService.getPhotos());
+  lightbox.refresh();
 }
 
-function clearGallery(){
-    refs.gallery.innerHTML = '';
+function clearGallery() {
+  refs.gallery.innerHTML = '';
 }
 
 function createMarkup({ data }) {
   console.log(data);
-  let markup = '';
-  data.hits.map(photoInfo => {
-    markup += `<a href='${photoInfo.largeImageURL}'>
-    <img src="${photoInfo.webformatURL}" alt="${photoInfo.tags}" loading="lazy" />
-    
-  </a>`;
-  });
+  let markup = data.hits
+    .map(photoInfo => {
+      return `<a class="gallery__item" href='${photoInfo.largeImageURL}'>
+    <img class="gallery__image" src="${photoInfo.webformatURL}" alt="${photoInfo.tags}" loading="lazy" />
+    <div class="info">
+    <p class="info-item">
+      <b>Likes</b>${photoInfo.likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>${photoInfo.views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>${photoInfo.comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>${photoInfo.downloads}
+    </p>
+  </div>
+    </a>`;
+    }).join('');
   refs.gallery.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
 }
 
-function onLoadMore() {
-  photoApiService.getPhotos();
+async function onLoadMore() {
+  createMarkup(await photoApiService.getPhotos());
+  lightbox.refresh();
 }
-
-
-{/* <div class="info">
-      <p class="info-item">
-        <b>Likes</b>${photoInfo.likes}
-      </p>
-      <p class="info-item">
-        <b>Views</b>${photoInfo.views}
-      </p>
-      <p class="info-item">
-        <b>Comments</b>${photoInfo.comments}
-      </p>
-      <p class="info-item">
-        <b>Downloads</b>${photoInfo.downloads}
-      </p>
-    </div> */}
